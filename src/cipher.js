bb7a6d6abded49ea0ecc0241b630d43baafa4d69
Daiane Vieira <dai.vieira07@yahoo.com.br>
2020-04-07 20:56:14 -0300

    Ajuste nos botões

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,33 @@
 const cipher = {
-  // ...
+  encode(deslocamento, texto) {
+    Number(deslocamento)
+
+    texto = texto.toUpperCase()
+    let messagearray = []
+    let textoRetorno = ""
+    
+    for(let i=0;i<texto.length;i++){
+      messagearray[i] = (texto.charAt(i))
+      messagearray[i] = (((messagearray[i].charCodeAt(0)) -65 + deslocamento) % 26 + 65)
+      textoRetorno += String.fromCharCode(messagearray[i]) 
+
+    }
+    return textoRetorno;
+  },
+
+
+
+
+/*if ((messageArray[i].charCodeAt(0) < 65 || messageArray[i].charCodeAt(0) > 90) ){
+ messageArray[i] = (messageArray[i].charCodeAt())*/
+
+
+
+
+  decode() {
+    let a = "oiiii"
+    return a
+  }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+
+var deslocamento = document.getElementById("deslocamento")
+
+
+let deslocamento1 = 1
+
+function less() {
+    if (deslocamento1 > 1) {
+        deslocamento1--;
+
+        deslocamento.innerHTML = deslocamento1
+    }
+}
+
+function more() {
+    if (deslocamento1 < 33) {
+        deslocamento1++;
+        
+        deslocamento.innerHTML = deslocamento1
+    }
+}
+
+
+let enviar = function () {
+    let deslocamento = document.getElementById("deslocamento").value
+    let formValor = document.getElementById("formValor")
+    let texto = document.getElementById("textoInicial").value
+    let retorno
+    if (formValor.valorRadio.value == "cifrar") {
+        retorno = cipher.encode(deslocamento, texto)
+    }
+    alert(retorno)
+}
+
+document.getElementById("btnLess").addEventListener("click", less)
+document.getElementById("btnMore").addEventListener("click", more)
+document.getElementById("botao").addEventListener("click", enviar)
+
+
+
+
+//console.log(cipher);
